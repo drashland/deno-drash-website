2b0043f8b14306450a6e27a7cd8560652df99ffe
drashbot <59404681+drashbot@users.noreply.github.com>
2021-04-05 21:03:19 -0400

    Update dependencies and bump version numbers across repo (#395)
    Co-authored-by: crookse <crookse@users.noreply.github.com>

--- deps.ts
+++ deps.ts
@@ -1,5 +1,5 @@
 export const decoder = new TextDecoder();
 export const encoder = new TextEncoder();
-export { Drash } from "https://deno.land/x/drash@v1.4.3/mod.ts";
+export { Drash } from "https://deno.land/x/drash@v1.4.4/mod.ts";
 export { configs } from "./configs.js";
 export { readLines } from "https://deno.land/std@0.92.0/io/mod.ts";
