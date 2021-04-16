c998836baa26b73388b0d0fcd05dba3e502c6654
Edward Bebbington <EdwardSBebbington@hotmail.com>
2021-04-16 11:59:43 +0100

    add subcommand, option and command tutorials

---
+++ src/modules/line-v1.x/vue/pages/tutorials/command.vue
@@ -0,0 +1,57 @@
+<script>
+export const resource = {
+  paths: ["/tutorials/command"],
+  meta: {
+    title: "Command",
+  }
+}
+
+export default {
+  data() {
+    return {
+      title: resource.meta.title,
+      toc: [
+        "Before You Get Started",
+        "Folder Structure End State",
+        "Steps",
+        "Verification",
+      ]
+    };
+  },
+}
+</script>
+
+<template lang="pug">
+page(
+  :base_url="this.$conf.line.base_url"
+  :title="title"
+  :toc="toc"
+)
+  h2-hash Before You Get Started
+  p In your file where you will create your CLI tool, <code>const fm = new Line({...})</code>, this is your command. You are registering quite a few properties that Line will use to create your CLI.
+  p What you install is your command, for example <code>fm</code> (taken from the quickstart), this becomes your command. When a user installs your CLI file (to install your CLI), Line will register the <code>--version, --help, -h</code> and </code>-v</code> options, alongside any subcommands you registered.
+  hr
+  h2-hash Instantiating An Instance Of Line
+  p Creating an instance of Line is the bread and butter of your CLI tool. The class takes an object that will help define your CLI:
+  ol
+    li
+      p <code>name</code> - What is the name of your CLI? This is where you define that. Say you wanted your CLI to be called "manager": <code>name: "manager"</code>. The name is what displays at the top of the <code>--help</code> output.
+      p <code>description</code> - This is the description of your CLI, how would you describe in a simple sentance? For example: <code>description: "Manage what ports are open</code>. The description is displayed underneath the name in the <code>--help</code> output.
+      p <code>command</code> - This is the name of your command. Take the quickstart for example, the value here would be "fm". This helps in Line displaying your command name alongside any information in the <code>--help</code> output.
+      p <code>subcommands</code> - This can hold a list of subcommands that can be used with your CLI. For more information on this, please refer to the subcommand tutorial.
+      p <code>version</code> - What version is your CLI tool? For example: <code>version: "v1.0.0"</code>.
+  hr
+  h2-hash Example
+  Here is an example of how a command will look:
+  code-block(title="app.ts" language="typescript")
+    | const l = new Line({
+    |   command: "fm",
+    |   name: "File Manager",
+    |   description: "File operations, such as reading and writing",
+    |   subcommands: [
+    |     Subcommand1,
+    |     Subcommand2,
+    |   ],
+    |   version: "v1.0.0",
+    | });
+</template>
---
+++ src/modules/line-v1.x/vue/pages/tutorials/subcommands.vue
@@ -0,0 +1,62 @@
+<script>
+export const resource = {
+  paths: ["/tutorials/subcommands"],
+  meta: {
+    title: "Subcommands",
+  }
+}
+
+export default {
+  data() {
+    return {
+      title: resource.meta.title,
+      toc: [
+        "Before You Get Started",
+        "Folder Structure End State",
+        "Steps",
+        "Verification",
+      ]
+    };
+  },
+}
+</script>
+
+<template lang="pug">
+page(
+  :base_url="this.$conf.line.base_url"
+  :title="title"
+  :toc="toc"
+)
+  h2-hash Before You Get Started
+  p A CLI wouldn't be complete without subcommands... Line offers the ability to plug in as many subcommands as you wish for your main command.
+  p A subcommand is responsible for the functionality of your CLI.Using the quickstart as an example, adding "read" and "write" subcommands to your main command will allow your CLI to process those: <code>fm read</code>.
+  hr
+  h2-hash Creating A Subcommand Class
+  p When you write a subcommand, it's the form of a class and is an extension onto Line's Subcommand class. Here is the list of properties and methods your subcommand classes can have and use:
+  ol
+    li
+      p <code>signature</code> - This will explain how the subcommand should be used, and will be used in the <code>--help</code> option. For example if my subcommand was to write the contents of one file to another, the signature would be: <code>public signature: "write [src] [dst]"</code> (where "dst" means destination, and "src" means source)
+      p <code>description</code> - The description of the subcommand, what will it do? What is the purpose? This will also be used in the <code>--help</code> option, for example: <code>public description: "Write the contents of one file to another"</code>.
+      p <code>options</code> - This property is what holds the options for the subcommand (can also be called "flags"). It can contain a list of options (classes that extend Line's Options class) should you wish to support options for a subcommand. For more information, please refer to the options tutorial.
+      p <code>handle()</code> - This method is the bread and butter of your subcommand. When you type <code>fm write file_1.ts file_2.ts</code>, this is what is invoked to handle the usage of the subcommand.
+  hr
+  h2-hash Example
+  p Here is an example of how a subcommand class would be created:
+  code-block(:header="false" language="typescript")
+    | class WriteCommand extends Subcommand {
+    |   public signature = "write [src] [dst]";
+    |   public description = "Write the contents of one file to another";
+    |   public options = [
+    |     Option1, // adds a `--some-required-option` option for this command
+    |   ];
+    |   public handle(): void {
+    |     if (this.getOptionValue("--some-required-option")) {
+    |       throw new Error("The `-some-required-option` option is required"); // or can log the help option
+    |     }
+    |
+    |     const source = this.getArgumentValue("src"); // matches the signature
+    |     const destination = this.getArgumentValue("dst"); // matches the signature
+    |     Deno.writeFileSync(source, destination);
+    |   }
+    | }
+</template>
