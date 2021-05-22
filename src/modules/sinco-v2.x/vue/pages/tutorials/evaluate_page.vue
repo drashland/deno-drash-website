5beb3af7a21dcc30d8764a81a75a9f7486e0bed8
Edward Bebbington <EdwardSBebbington@hotmail.com>
2021-05-22 22:24:06 +0100

    fix(sinco): sinco v2 evaluate docs fail

--- src/modules/sinco-v2.x/vue/pages/tutorials/evaluate_page.vue
+++ src/modules/sinco-v2.x/vue/pages/tutorials/evaluate_page.vue
@@ -67,7 +67,7 @@ page(
         |
         | Deno.test("My web app works as expected", async () => {
         |   const Sinco = await buildFor("chrome");
-        |   await Sinco.goTo("https://chromestatus.com");
+        |   await Sinco.goTo("https://drash.land");
         |   const pageTitle = await Sinco.evaluatePage(() => {
         |     return document.title;
         |   })
@@ -82,10 +82,10 @@ page(
         |     return document.body.children.length;
         |   })
         |   await Sinco.done();
-        |   assertEquals(pageTitle, "Chrome Platform Status");
+        |   assertEquals(pageTitle, "Drash Land");
         |   assertEquals(sum, 11);
-        |   assertEquals(oldBodyLength, 7);
-        |   assertEquals(newBodyLength, 8);
+        |   assertEquals(oldBodyLength, 14);
+        |   assertEquals(newBodyLength, 15);
         | })
       p Within the function you can pass to <code>evaluatePage()</code>, you have full access to the DOM, meaning you can write client side JavaScript like you normally would, for example:
       code-block(title="" language="typescript")
