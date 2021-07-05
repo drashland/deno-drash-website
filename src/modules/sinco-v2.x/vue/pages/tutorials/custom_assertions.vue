38d6c5c411ec456c1b8f778349ab9f6c356cd5fa
Edward Bebbington <47337480+ebebbington@users.noreply.github.com>
2021-07-05 18:35:02 +0100

    docs(sinco): Update custom assertions docs following changes to chromestatus.com (#424)

--- src/modules/sinco-v2.x/vue/pages/tutorials/custom_assertions.vue
+++ src/modules/sinco-v2.x/vue/pages/tutorials/custom_assertions.vue
@@ -60,12 +60,12 @@ page(
         |
         | Deno.test("My web app works as expected", async () => {
         |   const Sinco = await buildFor("chrome");
-        |   await Sinco.goTo("https://chromestatus.com");
-        |   await Sinco.assertUrlIs("https://chromestatus.com/features");
-        |   await Sinco.assertSee("Chrome versions");
+        |   await Sinco.goTo("https://drash.land");
+        |   await Sinco.assertUrlIs("https://drash.land/");
+        |   await Sinco.assertSee("Develop With Confidence");
         |   await Sinco.done();
         | })
-      p Here you are going to create your headless browser instance, and navigate to <code>https://chromestatus.com</code>. Once the page has loaded, you are going to assert that the page you are on is as expected. Note that <code>https://chromestatus.com</code> will redirect to <code>https://chromestatus.com/features</code>, which is why you have added the <code>/features</code> value to the end of the expected URL. You will assert that and also assert that you can see some given text on the page, and in this example, it is <code>Chrome Versions</code>.
+      p Here you are going to create your headless browser instance, and navigate to <code>https://drash.land</code>. Once the page has loaded, you are going to assert that the page you are on is as expected. You will assert this URL and also assert that you can see some given text on the page, and in this example, it is <code>Develop With Confidence</code>.
   hr
   h2-hash Verification
   ol
